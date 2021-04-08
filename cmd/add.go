8dd8ccf9128041a889e32ce716c343d2a508d1a6
이효빈 <ihyobin@ihyobin-ui-MacBookPro.local>
2021-04-08 17:16:00 +0900

    first commit

---
+++ cmd/add.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add values passed to function",
+	Long:  `Demo application to demonstrate cobra featues`,
+	Run: func(cmd *cobra.Command, args []string) {
+		sum := 0
+		for _, args := range args {
+			num, err := strconv.Atoi(args)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+			sum = sum + num
+		}
+		fmt.Println("result of addition is", sum)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(add)
+}
